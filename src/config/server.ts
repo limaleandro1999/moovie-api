c1e321cc1ab199e3cc2106660d553e61cb99c04b
limaleandro1999 <limaleandro1999@gmail.com>
2018-11-16 17:11:49 -0300

    added cors

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
 import { router } from './router'
 import { tokenParser } from '../components/security/token.parser'
 
@@ -7,6 +8,7 @@ import { initializeDb } from './db'
 
 const app: express.Application = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(tokenParser)
